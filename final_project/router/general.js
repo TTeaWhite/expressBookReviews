76d2098dfe73cfe9082c96bf0e8ef813e9f51310
TTeaWhite <21102437d@connect.polyu.hk>
2024-08-16 20:23:25 +0800

    Task2 update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,16 +11,22 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  // 返回所有书籍的信息
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn; // 获取请求参数中的 ISBN
+  const book = books[isbn]; // 根据 ISBN 查找书籍
+
+  if (book) {
+    return res.status(200).json(book); // 如果找到书籍，返回书籍信息
+  } else {
+    return res.status(404).json({ message: "书籍未找到" }); // 如果未找到书籍，返回 404 状态码和错误信息
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
